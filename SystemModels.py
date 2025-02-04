20feda4842561db0a9fa79df6312716eacdc90d3
XERN0N <sigurdmjn@gmail.com>
2025-02-04 10:39:52 +0100

    Added SciPy and Solver.py

--- SystemModels.py
+++ SystemModels.py
@@ -3,3 +3,10 @@ import numpy.typing as npt
 
 def chain(num_of_masses: int, end_connection: bool = False) -> tuple[npt.NDArray, npt.NDArray]:
     stifness_matrix = np.eye(num_of_masses)
+
+
+
+def mass_spring_damper(m: float, k: float, c: float) -> tuple[npt.NDArray, npt.NDArray]:
+    A = np.array([[0, 1], [-k/m, -c/m]])
+    B = np.array([[0], [1/m]])
+    return A, B
