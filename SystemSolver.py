e1e335abe70e90ace775290e66fdfec59e91c4d8
XERN0N <sigurdmjn@gmail.com>
2025-02-11 12:32:51 +0100

    added nonhomogenious solution to solver and fft to plotter

--- SystemSolver.py
+++ SystemSolver.py
@@ -1,14 +1,33 @@
 import numpy as np
 import numpy.typing as npt
+from typing import Callable
 from scipy.integrate import solve_ivp
 from SystemModels import chain
 
+def harmonic_load(t: float) -> float:
+    """
+    Defines a harmonic load as a function of time.
+
+    Parameters
+    ----------
+    t : float
+        The time point to evaluate the load at.
+
+    Returns
+    -------
+    float
+        The load at the given time point.
+    """
+    return np.sin(t)
+
 def equation_of_motions(
-    t: npt.ArrayLike,
-    y: npt.ArrayLike, 
-    M: npt.ArrayLike, 
-    C: npt.ArrayLike, 
-    K: npt.ArrayLike
+    t: float,
+    y: npt.NDArray, 
+    M: npt.NDArray, 
+    C: npt.NDArray, 
+    K: npt.NDArray,
+    B_2: npt.NDArray,
+    u: Callable[[float], npt.NDArray],
     ) -> tuple[npt.ArrayLike]:
     """
     Calculates the velocity and acceleration of the system at a given time.
@@ -26,76 +45,29 @@ def equation_of_motions(
         The damping matrix of the system.
     K : array_like with shape (n, n) (2D-vector)
         The stiffness matrix of the system.
+    B_2 : array_like with shape (n, n) (2D-vector)
+        The matrix for the external force mapping.
+    u : array_like with shape (n,) (1D-vector)
+        The external force applied to the system.
 
     Returns
     -------
     tuple of np arrays with shape (n,) (1D-vector)
         The velocity and acceleration of the system.
     """
+
     #Splitting the 1D input with [position, velocity] into two 1D arrays
     y, ydot = np.split(y, 2)
 
     # Equation of motion for the system
-    # [M]*y'' + [C]*y' + [K]*y = 0
+    # [M]*y'' + [C]*y' + [K]*y -[B_2]*u = 0
     # Isolate ddy/dt:
-    # y'' = [M]^-1 * (-[C]*y' - [K]*y)
+    # y'' = [M]^-1 * (-[C]*y' - [K]*y + [B_2]*u)
 
-    ydotdot = np.linalg.inv(M) @ (-C @ydot - K @ y)
+    ydotdot = np.linalg.inv(M) @ (-C @ ydot - K @ y + B_2 @ u(t))
 
     # Returning velocity and acceleration as a concatenated 1D array
     return np.concatenate((ydot, ydotdot))
-
-def solve_system(
-    y_ic: npt.ArrayLike,
-    t_duration: npt.ArrayLike,
-    M: npt.ArrayLike,
-    C: npt.ArrayLike,
-    K: npt.ArrayLike,
-    sol_method: str = 'RK45',
-    rel_tol: float = None,
-    t_eval: npt.ArrayLike = None
-) -> tuple[npt.ArrayLike, npt.ArrayLike]:
-    """
-    Solves the system of differential equations defined by the mass, damping, and stiffness matrices using chain function.
-
-    Parameters
-    ----------
-    y_ic : array_like with shape (n,) (1D-vector)
-        The initial conditions of the system.
-    t_duration : array_like with shape (2,) (1D-vector)
-        The time span for the solution.
-    M : array_like with shape (n, n) (2D-vector)
-        The mass matrix of the system.
-    C : array_like with shape (n, n) (2D-vector)
-        The damping matrix of the system.
-    K : array_like with shape (n, n) (2D-vector)
-        The stiffness matrix of the system.
-    sol_method : str, optional
-        The integration method to use (default is 'RK45').
-    rel_tol : float, optional
-        The relative tolerance for the solver (default is None).
-    t_eval : array_like, optional
-        The time points at which to store the computed solution (default is None).
-
-    Returns
-    -------
-    tuple of np arrays
-        The time points and the solution of the system.
-    """
-    if t_eval is not None:
-        Ivp_Results = solve_ivp(equation_of_motions, t_duration, y_ic, method=sol_method, rtol=rel_tol, t_eval=t_eval, args=(M, C, K))
-    elif sol_method and rel_tol is None:
-        Ivp_Results = solve_ivp(equation_of_motions, t_duration, y_ic, method=sol_method, args=(M, C, K))
-    elif rel_tol is None:
-        Ivp_Results = solve_ivp(equation_of_motions, t_duration, y_ic, method=sol_method, args=(M, C, K))
-    elif sol_method is None:
-        Ivp_Results = solve_ivp(equation_of_motions, t_duration, y_ic, method='RK45', rtol=rel_tol, args=(M, C, K))
-    else:
-        Ivp_Results = solve_ivp(equation_of_motions, t_duration, y_ic, method=sol_method, rtol=rel_tol, args=(M, C, K))
-    
-    return (Ivp_Results.t, Ivp_Results.y)
-
-
 
 #Example
 if __name__ == "__main__":
@@ -103,8 +75,17 @@ if __name__ == "__main__":
     t = (0, 10)
     t_points = np.linspace(*t, 1000)
     Mass, Damping, Stiffness = chain([1, 1, 1], [3, 2, 1], [0.2, 0.2, 0.2])
-    Results_t, Results_y = solve_system(y_0, t, Mass, Damping, Stiffness, 'RK23', 0.01, t_points)
-    
+    load = lambda t: np.array([1]) * np.sin(t*1)
+    load_map = np.array([[0, 0, 1]]).T
+    Results = solve_ivp(
+        fun=equation_of_motions, 
+        t_span=t, 
+        y0=y_0, 
+        args=(Mass, Damping, Stiffness, load_map, load),
+        t_eval=t_points
+    )
+    Results_y = Results.y
+    Results_t = Results.t
     combined_results = np.hstack((Results_y[:, :3], Results_y[:, -3:]))
     np.set_printoptions(precision=3)
     print(f"Combined results (6x??): \n{combined_results}")
